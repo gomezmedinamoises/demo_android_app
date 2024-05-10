e61bab3a73fd9b8fa69f61e18b5b3bffb2e9528e
moises <mac@Macs-MacBook-Pro.local>
2024-05-10 15:08:19 -0600

    Add unit test for MovieDetailsViewModel and MovieListViewModel

---
+++ app/src/test/java/com/example/demoapp/movie_list/presentation/details/MovieDetailsViewModelTest.kt
@@ -0,0 +1,8 @@
+package com.example.demoapp.movie_list.presentation.details
+
+/**
+ * @author Moises David Gomez Medina
+ */
+
+class MovieDetailsViewModelTest {
+}
---
+++ app/src/test/java/com/example/demoapp/movie_list/presentation/home/MovieListViewModelTest.kt
@@ -0,0 +1,8 @@
+package com.example.demoapp.movie_list.presentation.home
+
+/**
+ * @author Moises David Gomez Medina
+ */
+
+class MovieListViewModelTest {
+}
